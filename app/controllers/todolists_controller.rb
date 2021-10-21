4b81652663282038f75ce0b731fb2a61ec28fce3
EC2 Default User <ec2-user@ip-172-31-91-255.ec2.internal>
2021-10-21 15:39:28 +0000

    [Finish]2-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -30,5 +30,6 @@ def update
   private
   def list_params
     params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1f475c754c8bd7c7005cc91998ccaa9694b64cc8e1b61d291d90b329e25fe9f9f8dadf1fd190a678ecd715768592ede4a9680450333b94ebfc86984ac3fdf9d0'
